0bdc9a2f8b85047027f24c944a3a8a66b98cdd70
humphrey kiriama <humphrey.kiriama@gmail.com>
2022-09-28 03:07:42 -0700

    t1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
+
 def safe_print_integer(value):
-
     try:
-
         print("{:d}".format(value))
-
         return True
-
-    except Exception as error:
-
-        return 
+    except (ValueError, TypeError):
+        return False
