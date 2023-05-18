5e85bec271c50e03190d92d7d8858a1d110cab36
hkiriama <humphrey.kiriama@gmail.com>
2023-05-18 02:49:58 -0700

    inheritance

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+
+dule for MyList class '''
+
+
+class MyList(list):
+    ''' Custom list class'''
+
+    def print_sorted(self):
+        ''' Instance method:
+            print sorted list
+        '''
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/1-my_list.txt")
