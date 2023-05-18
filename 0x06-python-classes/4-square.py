98f977d2c99e13713e9b2521fb7b87c95051970d
hkiriama <humphrey.kiriama@gmail.com>
2023-05-17 23:38:41 -0700

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""First definition of a class"""
+
+
+class Square():
+    """class Square"""
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
