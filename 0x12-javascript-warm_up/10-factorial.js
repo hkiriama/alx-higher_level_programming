a75254bf467cc5773c44db95d6bafc49df829afe
hkiriama <humphrey.kiriama@gmail.com>
2023-05-19 03:54:25 +0000

    javascript

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+let numIni = parseInt(process.argv[2]);
+function numFact (numIni) {
+  if (numIni > 0) return (numIni * numFact(numIni - 1));
+  return (1);
+}
+if (isNaN(numIni)) numIni = 1;
+console.log(numFact(numIni));
