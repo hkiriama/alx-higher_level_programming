bee71c0979718eb40cf82951fd9c12019d05f191
hkiriama <humphrey.kiriama@gmail.com>
2023-05-23 12:04:42 -0700

    network

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Write Bash script x header variable X-School must be sent with the value 98
+sudo curl -s -X GET $1 -H "X-School-User-Id: 98"#!/bin/bash
+# Write Bash script x header variable X-School must be sent with the value 98
+sudo curl -s -X GET $1 -H "X-School-User-Id: 98"
